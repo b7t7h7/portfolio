9a10fa65cb4c07b8ec5d20a4158d8cfde75d9b57
b7t7h7 <61854010+b7t7h7@users.noreply.github.com>
2020-08-22 14:08:04 -0400

    resumehref

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -16,7 +16,7 @@ function PortNav() {
             <Link to="/Portfolio/">Portfolio</Link>
             <Nav.Link href="https://github.com/b7t7h7" target="_blank">GitHub</Nav.Link>
             <Nav.Link href="https://www.linkedin.com/in/benjamin-huntley-ba628a1a9/" target="_blank">LinkedIn</Nav.Link>
-            <Nav.Link href="/Portfolio/assets/Ben_Huntley_Resume.docx" target="_blank">Resume</Nav.Link>
+            <Nav.Link href="portfolio/public/assets/Ben_Huntley_Resume.docx" target="_blank">Resume</Nav.Link>
             {/* <Link to={{ pathname: "https://example.zendesk.com/hc/en-us/articles/123456789-Privacy-Policies" }} target="_blank" /> */}
           </Nav>
         </Navbar.Collapse>
